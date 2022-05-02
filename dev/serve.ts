5f17a15bd8bfe9826badf7bde8b7f73314759553
rizukikhair4658 <rizkikhair4658@gmail.com>
2022-05-02 21:45:27 +0700

    fix: split between global and single plugin

--- dev/serve.ts
+++ dev/serve.ts
@@ -1,11 +1,5 @@
 import { createApp } from 'vue';
 import Dev from './serve.vue';
-import VueFacebookMessanger from '@/index';
 
 const app = createApp(Dev);
-app.use(VueFacebookMessanger.installChat, {
-  page_id: 109209978435935, //  change 'null' to your Facebook Page ID,
-  theme_color: '#333333', // theme color in HEX
-  locale: 'en_US'
-})
 app.mount('#app');
